b919d9581d9b71864d34cf6fae9803a7d1255732
Miroslav <34798837+miskin1991@users.noreply.github.com>
2023-05-11 13:38:17 +0000

    Excercise 1: Basic stack operations

---
+++ stacks_and_queues/excercise/1_basic_stack_operations/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <sstream>
+#include <queue>
+#include <stack>
+
+
+int main()
+{
+    using namespace std;
+    string input;
+    int totalNumbers, popNumbers, searchNumber;
+    stack<int> numbers;
+
+    cin >> totalNumbers >> popNumbers >> searchNumber;
+
+    for (int i = 0; i < totalNumbers; i++)
+    {
+        int number;
+        cin >> number;
+        numbers.push(number);
+    }
+
+    for (int i = 0; i < popNumbers; i++)
+        numbers.pop();
+
+    if (numbers.empty())
+    {
+        cout << "0" << endl;
+        return 0;
+    }
+    
+    int minimum = INT32_MAX;
+    while(numbers.size())
+    {
+        int number = numbers.top();
+        numbers.pop();
+        if (number == searchNumber)
+        {
+            cout << "true" << endl;
+            return 0;
+        }
+        if (number < minimum)
+            minimum = number;
+    }
+
+    cout << minimum << endl;
+
+    return 0;
+}
