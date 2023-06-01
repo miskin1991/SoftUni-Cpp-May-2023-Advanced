c0dec9db1262fd16469ebcfa02efac5a116ccf8e
Miroslav <34798837+miskin1991@users.noreply.github.com>
2023-06-01 16:13:46 +0000

    2. Remove Negative and Reverse

---
+++ memory_management/2_remove_negative_and_reverse/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    int* arr = new int[1000];
+    string input;
+    getline(cin, input);
+    stringstream sstr(input);
+    int value;
+
+    int elements = 0;
+    while (sstr >> value ) {
+        arr[elements] = value;
+        elements++;
+    }
+
+    bool print = false;
+    for (int j = elements - 1; j > -1; j--) {
+        if (arr[j] > 0) {
+            cout << arr[j] << ' ';
+            print = true;
+        }
+    }
+
+    if (!print)
+        cout << "empty";
+    cout << endl;
+
+    return 0;
+}
